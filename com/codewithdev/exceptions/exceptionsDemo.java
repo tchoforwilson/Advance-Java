1f8a75ece83ea6a582ee630fe786130f258b7b20
tchoforwilson <tchoforwilson@gmail.com>
2024-09-27 21:34:14 +0100

    The try-with-resources statement

--- com/codewithdev/exceptions/exceptionsDemo.java
+++ com/codewithdev/exceptions/exceptionsDemo.java
@@ -2,23 +2,17 @@ package com.codewithdev.exceptions;
 
 import java.io.IOException;
 import java.io.FileReader;
+import java.io.FileWriter;
 
 public class ExceptionsDemo {
     public static void show() {
-        FileReader reader = null;
-        try {
-            reader = new FileReader("file.txt");
+        try (
+                var reader = new FileReader("file.txt");
+                var writer = new FileWriter("some.txt")) {
             var value = reader.read();
 
         } catch (IOException e) {
             System.out.println("Could not read data.");
-        } finally {
-            if (reader != null)
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
         }
     }
 }
