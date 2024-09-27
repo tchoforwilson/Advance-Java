71ec23480561736858109cda4bd1a69313793a5a
tchoforwilson <tchoforwilson@gmail.com>
2024-09-27 21:24:17 +0100

    Catching multiple exceptions

--- com/codewithdev/exceptions/exceptionsDemo.java
+++ com/codewithdev/exceptions/exceptionsDemo.java
@@ -1,14 +1,20 @@
 package com.codewithdev.exceptions;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.io.FileReader;
 
 public class ExceptionsDemo {
     public static void show() {
         try {
             var reader = new FileReader("file.txt");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            var value = reader.read();
+            new SimpleDateFormat().parse("null");
+
+        } catch (IOException | ParseException e) {
+            System.out.println("Could not read data.");
         }
     }
 }
