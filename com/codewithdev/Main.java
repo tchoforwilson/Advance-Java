3266eec98c0043180b992cd5720abb048944cda2
tchoforwilson <tchoforwilson@gmail.com>
2024-09-29 21:24:51 +0100

    A poor solution

--- com/codewithdev/Main.java
+++ com/codewithdev/Main.java
@@ -1,10 +1,15 @@
 package com.codewithdev;
 
 import com.codewithdev.generics.List;
+import com.codewithdev.generics.User;
 
 public class Main {
     public static void main(String[] args) {
         var list = new List();
-        list.add(1);
+        list.add(Integer.valueOf(1));
+        list.add("1");
+        list.add(new User());
+
+        int number = (int) list.get(2);
     }
 }
