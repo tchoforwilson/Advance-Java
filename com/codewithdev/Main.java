c03979b487ae9d930999f3e0f6c8c87493d90f47
tchoforwilson <tchoforwilson@gmail.com>
2024-09-30 10:01:02 +0100

    Multiple type parameters

--- com/codewithdev/Main.java
+++ com/codewithdev/Main.java
@@ -6,7 +6,6 @@ import com.codewithdev.generics.Utils;
 
 public class Main {
     public static void main(String[] args) {
-        var max = Utils.max(new User(10), new User(20));
-        System.out.println(max);
+        Utils.print(1, 10);
     }
 }
