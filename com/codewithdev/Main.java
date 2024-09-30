427eadb66eec14214b152d32c95eb5aef7b3f2f8
tchoforwilson <tchoforwilson@gmail.com>
2024-09-30 10:12:53 +0100

    Generic Classes and Inheritance

--- com/codewithdev/Main.java
+++ com/codewithdev/Main.java
@@ -1,11 +1,14 @@
 package com.codewithdev;
 
 import com.codewithdev.generics.GenericList;
+import com.codewithdev.generics.Instructor;
 import com.codewithdev.generics.User;
 import com.codewithdev.generics.Utils;
 
 public class Main {
     public static void main(String[] args) {
-        Utils.print(1, 10);
+        var instructors = new GenericList<Instructor>();
+        var users = new GenericList<User>();
+        Utils.printUsers(users);
     }
 }
--- com/codewithdev/generics/Utils.java
+++ com/codewithdev/generics/Utils.java
@@ -1,5 +1,8 @@
 package com.codewithdev.generics;
 
+import com.codewithdev.generics.User;
+import com.codewithdev.generics.GenericList;
+
 public class Utils {
     public static <T extends Comparable<T>> T max(T first, T second) {
         return first.compareTo(second) < 0 ? second : first;
@@ -7,5 +10,13 @@ public class Utils {
 
     public static <K, V> void print(K key, V value) {
         System.out.println(key + ": " + value);
+    }
+
+    public static void printUser(User user) {
+        System.out.println(user);
+    }
+
+    public static void printUsers(GenericList<User> users) {
+
     }
 }
