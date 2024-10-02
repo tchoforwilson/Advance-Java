a9a3b72833f7ca2667ef7f796724833d958b3c2f
tchoforwilson <tchoforwilson@gmail.com>
2024-10-02 21:16:03 +0100

    The Iterable Interface

--- com/codewithdev/Main.java
+++ com/codewithdev/Main.java
@@ -5,10 +5,8 @@ import com.codewithdev.generics.GenericList;
 public class Main {
     public static void main(String[] args) {
         var list = new GenericList<String>();
-        list.add("a");
-        list.add("b");
 
-        for (var item : list.items)
+        for (var item : list)
             System.out.println(item);
     }
 }
--- com/codewithdev/generics/GenericList.java
+++ com/codewithdev/generics/GenericList.java
@@ -1,7 +1,9 @@
 package com.codewithdev.generics;
 
-public class GenericList<T> {
-    public T[] items = (T[]) new Object[10];
+import java.util.Iterator;
+
+public class GenericList<T> implements Iterable<T> {
+    private T[] items = (T[]) new Object[10];
     private int count;
 
     public void add(T item) {
@@ -10,5 +12,10 @@ public class GenericList<T> {
 
     public T get(int index) {
         return items[index];
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
     }
 }
