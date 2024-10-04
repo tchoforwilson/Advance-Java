0b450f0391a1f3c73bdc0d2609c1ee534b958acf
tchoforwilson <tchoforwilson@gmail.com>
2024-10-04 22:38:41 +0100

    The comparable interface

--- com/codewithdev/Main.java
+++ com/codewithdev/Main.java
@@ -1,9 +1,19 @@
 package com.codewithdev;
 
-import com.codewithdev.collections.ListDemo;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import com.codewithdev.collections.Customer;
 
 public class Main {
     public static void main(String[] args) {
-        ListDemo.show();
+        List<Customer> customers = new ArrayList<Customer>();
+
+        customers.add(new Customer("James"));
+        customers.add(new Customer("Brown"));
+        customers.add(new Customer("Mary"));
+
+        Collections.sort(customers);
+        System.out.println(customers);
     }
 }
