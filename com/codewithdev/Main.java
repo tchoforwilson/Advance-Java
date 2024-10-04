538e633fa4efc5e92d4442d11134465f4031c465
tchoforwilson <tchoforwilson@gmail.com>
2024-10-04 22:58:05 +0100

    The Queue Interface

--- com/codewithdev/Main.java
+++ com/codewithdev/Main.java
@@ -1,20 +1,9 @@
 package com.codewithdev;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Collections;
-import com.codewithdev.collections.Customer;
-import com.codewithdev.collections.EmailComparator;
+import com.codewithdev.collections.QueueDemo;
 
 public class Main {
     public static void main(String[] args) {
-        List<Customer> customers = new ArrayList<Customer>();
-
-        customers.add(new Customer("James", "james@example.com"));
-        customers.add(new Customer("Brown", "rown@example.com"));
-        customers.add(new Customer("Mary", "ary@example.com"));
-
-        Collections.sort(customers, new EmailComparator());
-        System.out.println(customers);
+        QueueDemo.show();
     }
 }
---
+++ com/codewithdev/collections/QueueDemo.java
@@ -0,0 +1,17 @@
+package com.codewithdev.collections;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class QueueDemo {
+    public static void show() {
+        Queue<String> queue = new ArrayDeque<>();
+        queue.add("c");
+        queue.add("a");
+        queue.add("b");
+        // b -> a -> c
+        var front = queue.remove();
+        System.out.println(front);
+        System.out.println(queue);
+    }
+}
