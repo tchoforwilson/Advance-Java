2c06118291b9f84c47348df055c5732b564839f8
tchoforwilson <tchoforwilson@gmail.com>
2024-10-04 22:29:35 +0100

    The List interface

--- com/codewithdev/collections/ListDemo.java
+++ com/codewithdev/collections/ListDemo.java
@@ -7,9 +7,12 @@ import java.util.List;
 public class ListDemo {
     public static void show() {
         List<String> list = new ArrayList<String>();
-        Collections.addAll(list, "a", "b", "c");
+        Collections.addAll(list, "a", "b", "c", "a");
         list.set(0, "a+");
         list.remove(0);
         System.out.println(list.get(0));
+        System.out.println(list.indexOf("a"));
+        System.out.println(list.lastIndexOf("a"));
+        System.out.println(list.subList(0, 2));
     }
 }
