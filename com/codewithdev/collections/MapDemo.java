00e65cf66b9177c32e6dbc7e8ab02b26c37ee201
tchoforwilson <tchoforwilson@gmail.com>
2024-10-05 19:12:55 +0100

    Hash Tables

---
+++ com/codewithdev/collections/MapDemo.java
@@ -0,0 +1,14 @@
+package com.codewithdev.collections;
+
+public class MapDemo {
+    public static void show() {
+        // Java: Maps
+        // C#: Dictionary
+        // Python: Dictionary
+        // JavaScript: Objects
+        // var person = { name: "a"}
+        /*
+         * Javascript objects are hash tables under the hood.
+         */
+    }
+}
