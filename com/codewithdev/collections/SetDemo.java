4f569992a012a4cc746ec3d0a0863de2dd415361
tchoforwilson <tchoforwilson@gmail.com>
2024-10-05 18:56:26 +0100

    Union of sets

--- com/codewithdev/collections/SetDemo.java
+++ com/codewithdev/collections/SetDemo.java
@@ -4,9 +4,11 @@ import java.util.*;
 
 public class SetDemo {
     public static void show() {
-        Collection<String> collection = new ArrayList<>();
-        Collections.addAll(collection, "a", "b", "c", "c");
-        Set<String> set = new HashSet<>(collection);
-        System.out.println(set);
+        Set<String> set1 = new HashSet<>(Arrays.asList("a", "b", "c"));
+        Set<String> set2 = new HashSet<>(Arrays.asList("b", "c", "d"));
+        // Union
+        set1.addAll(set2);
+        System.out.println(set1);
+
     }
 }
