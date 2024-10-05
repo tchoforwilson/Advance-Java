4ba942a1ffcb130911f38fb0957e3d272908f2b7
tchoforwilson <tchoforwilson@gmail.com>
2024-10-05 18:57:56 +0100

    Sets intersections

--- com/codewithdev/collections/SetDemo.java
+++ com/codewithdev/collections/SetDemo.java
@@ -6,9 +6,8 @@ public class SetDemo {
     public static void show() {
         Set<String> set1 = new HashSet<>(Arrays.asList("a", "b", "c"));
         Set<String> set2 = new HashSet<>(Arrays.asList("b", "c", "d"));
-        // Union
-        set1.addAll(set2);
+        // Intersection
+        set1.retainAll(set2);
         System.out.println(set1);
-
     }
 }
