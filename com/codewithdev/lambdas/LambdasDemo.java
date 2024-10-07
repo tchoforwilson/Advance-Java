280a692c1ae5a38760de66438ec378fde723a5b0
tchoforwilson <tchoforwilson@gmail.com>
2024-10-07 21:42:36 +0100

    Method references

--- com/codewithdev/lambdas/LambdasDemo.java
+++ com/codewithdev/lambdas/LambdasDemo.java
@@ -1,11 +1,13 @@
 package com.codewithdev.lambdas;
 
 public class LambdasDemo {
-    public String prefix = "-";
+    public LambdasDemo(String message) {
 
-    public void show() {
+    }
 
-        greet(message -> System.out.println(prefix + message));
+    public static void show() {
+        greet(LambdasDemo::new);
+        greet(LambdasDemo::new);
     }
 
     public static void greet(Printer printer) {
