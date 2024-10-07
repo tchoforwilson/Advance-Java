3a4ad761b56df5b6966d23c88ec88fef69488fe6
tchoforwilson <tchoforwilson@gmail.com>
2024-10-07 22:09:01 +0100

    Chaining Consumer

--- com/codewithdev/lambdas/LambdasDemo.java
+++ com/codewithdev/lambdas/LambdasDemo.java
@@ -1,17 +1,15 @@
 package com.codewithdev.lambdas;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public class LambdasDemo {
 
     public static void show() {
-        List<Integer> list = List.of(1, 2, 3);
+        List<String> list = List.of("a", "b", "c");
+        Consumer<String> print = item -> System.out.println(item);
+        Consumer<String> printUpperCase = item -> System.out.println(item.toUpperCase());
 
-        // Imperative Programming (for, if/else, switch/case)
-        for (var item : list)
-            System.out.println(item);
-
-        // Declarative Programming
-        list.forEach((item) -> System.out.println(item));
+        list.forEach(print.andThen(printUpperCase).andThen(print));
     }
 }
