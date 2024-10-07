2474299f0e363e852a1d3f5e047f36f7f5082b70
tchoforwilson <tchoforwilson@gmail.com>
2024-10-07 21:25:56 +0100

    Anonymous inner classes

--- com/codewithdev/lambdas/LambdasDemo.java
+++ com/codewithdev/lambdas/LambdasDemo.java
@@ -2,7 +2,12 @@ package com.codewithdev.lambdas;
 
 public class LambdasDemo {
     public static void show() {
-        greet(new ConsolePrinter());
+        greet(new Printer() {
+            @Override
+            public void print(String message) {
+                System.out.println(message);
+            }
+        });
     }
 
     public static void greet(Printer printer) {
