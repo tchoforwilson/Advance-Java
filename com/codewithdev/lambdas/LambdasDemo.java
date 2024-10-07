80c7905ac1b2a76f3c3d9df3fd852e101b61033d
tchoforwilson <tchoforwilson@gmail.com>
2024-10-07 22:13:07 +0100

    The supplier Interface

--- com/codewithdev/lambdas/LambdasDemo.java
+++ com/codewithdev/lambdas/LambdasDemo.java
@@ -1,15 +1,12 @@
 package com.codewithdev.lambdas;
 
-import java.util.List;
-import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class LambdasDemo {
 
     public static void show() {
-        List<String> list = List.of("a", "b", "c");
-        Consumer<String> print = item -> System.out.println(item);
-        Consumer<String> printUpperCase = item -> System.out.println(item.toUpperCase());
-
-        list.forEach(print.andThen(printUpperCase).andThen(print));
+        Supplier<Double> getRandom = () -> Math.random();
+        var random = getRandom.get();
+        System.out.println(random);
     }
 }
