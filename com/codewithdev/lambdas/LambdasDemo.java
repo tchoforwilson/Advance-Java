0963ffe8ca69aae05fa00977e0bb454ccc7278d9
tchoforwilson <tchoforwilson@gmail.com>
2024-10-08 20:11:12 +0100

    Composing Functions

--- com/codewithdev/lambdas/LambdasDemo.java
+++ com/codewithdev/lambdas/LambdasDemo.java
@@ -1,13 +1,20 @@
 package com.codewithdev.lambdas;
 
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 public class LambdasDemo {
 
     public static void show() {
-        Function<String, Integer> map = str -> str.length();
-        var length = map.apply("Sky");
-        System.out.println(length);
+        // "key:value"
+        // first: "key=value"
+        // second: "{key:value}"
+        Function<String, String> replaceColon = str -> str.replace(":", "=");
+        Function<String, String> addBraces = str -> "{" + str + "}";
+
+        // Declarative Programming
+        var result = replaceColon.andThen(addBraces).apply("key:value");
+
+        result = addBraces.compose(replaceColon).apply("key:value");
+        System.out.println(result);
     }
 }
