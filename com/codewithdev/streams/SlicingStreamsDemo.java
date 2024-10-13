b47aaed380fb9a76f8f39ea97e87fa79fcbc2b19
tchoforwilson <tchoforwilson@gmail.com>
2024-10-13 20:21:05 +0100

    Slicing Streams

--- com/codewithdev/streams/SlicingStreamsDemo.java
+++ com/codewithdev/streams/SlicingStreamsDemo.java
@@ -6,14 +6,10 @@ public class SlicingStreamsDemo {
     public static void show() {
         List<Movie> movies = List.of(
                 new Movie("Terminator", 10),
-                new Movie("Avatar", 15),
+                new Movie("Avatar", 30),
                 new Movie("Secreet Road", 20));
 
-        // 1000 movies
-        // 10 movies per page
-        // 3rd page
-        // skip(20) = skip((page - 1) x pageSize)
-        // limit(10) = limit(pageSize)
-        movies.stream().skip(2).forEach(movie -> System.out.println(movie.getTitle()));
+        movies.stream().dropWhile(movie -> movie.getLikes() < 30)
+                .forEach(movie -> System.out.println(movie.getTitle()));
     }
 }
