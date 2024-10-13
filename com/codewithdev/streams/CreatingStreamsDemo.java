3dc0959d5d656add30ec00d766c25d193acf21b9
tchoforwilson <tchoforwilson@gmail.com>
2024-10-13 19:50:27 +0100

    Limiting values in streams

--- com/codewithdev/streams/CreatingStreamsDemo.java
+++ com/codewithdev/streams/CreatingStreamsDemo.java
@@ -2,10 +2,11 @@ package com.codewithdev.streams;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.stream.Stream;
 
 public class CreatingStreamsDemo {
     public static void show() {
-        int[] numbers = { 1, 2, 3 };
-        Arrays.stream(numbers).forEach(n -> System.out.println(n));
+        var stream = Stream.generate(() -> Math.random());
+        stream.limit(3).forEach(e -> System.out.println(e));
     }
 }
