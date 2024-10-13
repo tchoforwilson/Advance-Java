410759a23327a301909fa2bbeb557de518140133
tchoforwilson <tchoforwilson@gmail.com>
2024-10-13 19:51:52 +0100

    Creating a Stream

--- com/codewithdev/streams/CreatingStreamsDemo.java
+++ com/codewithdev/streams/CreatingStreamsDemo.java
@@ -6,7 +6,6 @@ import java.util.stream.Stream;
 
 public class CreatingStreamsDemo {
     public static void show() {
-        var stream = Stream.generate(() -> Math.random());
-        stream.limit(3).forEach(e -> System.out.println(e));
+        Stream.iterate(1, n -> n + 1).limit(10).forEach(n -> System.out.println(n));
     }
 }
