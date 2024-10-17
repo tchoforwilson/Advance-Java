cc50c0c8a465c216cd591570a1876da051359a28
tchoforwilson <tchoforwilson@gmail.com>
2024-10-17 19:56:11 +0100

    Pausing a thread

--- com/codewithdev/concurrency/DownloadFileTask.java
+++ com/codewithdev/concurrency/DownloadFileTask.java
@@ -5,6 +5,14 @@ public class DownloadFileTask implements Runnable {
     @Override
     public void run() {
         System.out.println("Downloading a file: " + Thread.currentThread().getName());
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Download complete: " + Thread.currentThread().getName());
     }
 
 }
