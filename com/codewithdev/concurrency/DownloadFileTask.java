3f366e4243c51f4149cbd2a361071a119a27cc3f
tchoforwilson <tchoforwilson@gmail.com>
2024-10-19 10:11:18 +0100

    Thread Signalling with wait() and notify() methods

--- com/codewithdev/concurrency/DownloadFileTask.java
+++ com/codewithdev/concurrency/DownloadFileTask.java
@@ -22,6 +22,9 @@ public class DownloadFileTask implements Runnable {
         }
 
         status.done();
+        synchronized (status) {
+            status.notifyAll();
+        }
 
         System.out.println("Download complete: " + Thread.currentThread().getName());
     }
--- com/codewithdev/concurrency/ThreadDemo.java
+++ com/codewithdev/concurrency/ThreadDemo.java
@@ -9,7 +9,14 @@ public class ThreadDemo {
 
         var thread1 = new Thread(new DownloadFileTask(status));
         var thread2 = new Thread(() -> {
-            while (status.isDone()) {
+            while (!status.isDone()) {
+                synchronized (status) {
+                    try {
+                        status.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
             System.out.println(status.getTotalBytes());
         });
