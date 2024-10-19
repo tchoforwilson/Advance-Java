e61b6daf81b59e9e0d8164776a005b457599ac20
tchoforwilson <tchoforwilson@gmail.com>
2024-10-19 19:42:07 +0100

    Implementing an Asynchronous API

--- com/codewithdev/Main.java
+++ com/codewithdev/Main.java
@@ -1,9 +1,18 @@
 package com.codewithdev;
 
 import com.codewithdev.executors.CompletableFuturesDemo;
+import com.codewithdev.executors.MailService;
 
 public class Main {
     public static void main(String[] args) {
-        CompletableFuturesDemo.show();
+        var service = new MailService();
+        service.sendAsync();
+        System.out.println("Hello World!");
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
