f1f0b271909a485dd84568f08c66176cafa9f0a6
tchoforwilson <tchoforwilson@gmail.com>
2024-10-19 09:20:08 +0100

    The synchronized Keyword which is advisable not to use

--- com/codewithdev/concurrency/DownloadStatus.java
+++ com/codewithdev/concurrency/DownloadStatus.java
@@ -1,22 +1,28 @@
 package com.codewithdev.concurrency;
 
-import java.util.concurrent.locks.ReentrantLock;
-
 public class DownloadStatus {
     private int totalBytes;
-    private ReentrantLock lock = new ReentrantLock();
+    private int totalFiles;
+    private Object totalBytesLock = new Object();
+    private Object totalFilesLock = new Object();
+
+    public void incrementTotalBytes() {
+        synchronized (totalBytesLock) {
+            totalBytes++;
+        }
+    }
+
+    public void incrementTotalFiles() {
+        synchronized (totalFilesLock) {
+            totalFiles++;
+        }
+    }
+
+    public int getTotalFiles() {
+        return totalFiles;
+    }
 
     public int getTotalBytes() {
         return totalBytes;
-    }
-
-    public void incrementTotalBytes() {
-        lock.lock();
-        try {
-            totalBytes++;
-        } finally {
-
-            lock.unlock();
-        }
     }
 }
