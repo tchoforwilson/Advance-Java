dbfa2702c15b8abc46f6fadb18803a7d1d047094
tchoforwilson <tchoforwilson@gmail.com>
2024-10-19 20:00:31 +0100

    Handling Exceptions

--- com/codewithdev/executors/CompletableFuturesDemo.java
+++ com/codewithdev/executors/CompletableFuturesDemo.java
@@ -7,15 +7,17 @@ import java.util.function.Supplier;
 
 public class CompletableFuturesDemo {
     public static void show() {
-        var future = CompletableFuture.supplyAsync(() -> 1);
-        future.thenAcceptAsync((result) -> {
-            System.out.println(Thread.currentThread().getName());
-            System.out.println(result);
+        var future = CompletableFuture.supplyAsync(() -> {
+            System.out.println("Getting the current weather");
+            throw new IllegalStateException();
         });
 
         try {
-            Thread.sleep(2000);
+            var temperature = future.exceptionally(ex -> 1).get();
+            System.out.println(temperature);
         } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
             e.printStackTrace();
         }
     }
