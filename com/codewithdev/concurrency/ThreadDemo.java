7772563d7bbaee3e6a76bc15fd8d24040e682dbf
tchoforwilson <tchoforwilson@gmail.com>
2024-10-19 10:36:50 +0100

    Concurrent Collections

--- com/codewithdev/concurrency/ThreadDemo.java
+++ com/codewithdev/concurrency/ThreadDemo.java
@@ -4,28 +4,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ThreadDemo {
     public static void show() {
-        Collection<Integer> collection = Collections.synchronizedCollection(new ArrayList<>());
+        Map<Integer, String> map = new ConcurrentHashMap<>();
 
-        var thread1 = new Thread(() -> {
-            collection.addAll(Arrays.asList(1, 2, 3));
-        });
-        var thread2 = new Thread(() -> {
-            collection.addAll(Arrays.asList(4, 5, 6));
-        });
+        map.put(1, "a");
+        map.get(1);
+        map.remove(1);
 
-        thread1.start();
-        thread2.start();
-
-        try {
-            thread1.join();
-            thread2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println(collection);
+        System.out.println(map);
     }
 }
