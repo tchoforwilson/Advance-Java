4e45ca07ecebe1e8f3918d0e59b3a3e1b70a3219
tchoforwilson <tchoforwilson@gmail.com>
2024-10-19 09:11:49 +0100

    Using locks to prevent concurrency problems

--- com/codewithdev/concurrency/DownloadStatus.java
+++ com/codewithdev/concurrency/DownloadStatus.java
@@ -1,13 +1,22 @@
 package com.codewithdev.concurrency;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class DownloadStatus {
     private int totalBytes;
+    private ReentrantLock lock = new ReentrantLock();
 
     public int getTotalBytes() {
         return totalBytes;
     }
 
     public void incrementTotalBytes() {
-        totalBytes++;
+        lock.lock();
+        try {
+            totalBytes++;
+        } finally {
+
+            lock.unlock();
+        }
     }
 }
